c72c7ef5f4c8a4c8010f8ba6194911b5cbf49001
PeterAkande <akandepeter86@gmail.com>
2022-04-05 21:33:17 +0100

    First Wella Health Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WeekTwoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Akande Peter Akande, I am in the BackEnd class of Wella Schools");
+            Console.WriteLine("I am a Mobile Application develper and many a times, i would always want to build custom backends for my Applications\nOn sighting this opportunity, i knew the time was right for me to get started and start building my own backends and APIs");
+        }
+    }
+}
